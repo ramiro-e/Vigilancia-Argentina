43fc45fb7cf9ec7dbdb6232386395be795bbf878
ramiro-e <ramiroestevez96@gmail.com>
2021-08-10 20:31:09 -0300

    Homepage V1

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const fs = require("fs")
 
 module.exports = {
     index: (req,res)=>{
         res.render(path.resolve(__dirname, '../views/main/index'), {
             styles: ["header.css","index.css","footer.css"],
-            title: "Vigilancia Argentina"
+            title: "Vigilancia Argentina",
+            slider: fs.readdirSync(path.resolve(__dirname, '../../public/images/web/sliderHeader')),
+            productos: ["producto","producto","producto","producto"]
         })
     }
 }
