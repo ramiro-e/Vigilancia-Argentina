2cdd45dbfde84e8c06f2f04dc48ebac2729977f2
ramiro-e <ramiroestevez96@gmail.com>
2021-12-06 20:19:19 +0100

    vistas de administrador de productos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "vigilancia_argentina",
+    "database": "vigilancia_argentina_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
