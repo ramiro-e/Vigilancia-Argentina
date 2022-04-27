140cb07da062f6c3fc1a62fc1422d7aa2323735a
ramiro-e <ramiroestevez96@gmail.com>
2022-04-27 20:19:19 +0100

    app js changes

--- app.js
+++ app.js
@@ -4,21 +4,29 @@ const app = express();
 
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const cors = require('cors');
 
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser'); 
 
 const acceso = require('./src/middlewares/acceso');
 
 const mainRouter = require('./src/routes/mainRouter');
-const userRouter = require('./src/routes/userRouter');
 const productRouter = require('./src/routes/productRouter');
+const userRouter = require('./src/routes/userRouter');
+const shoppingCartRouter = require('./src/routes/shoppingCartRouter');
+
+const adminProductRoutes = require('./src/routes/admin/adminProductRoutes');
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(cors());
 
 
 //Middlewares que levantan el servidor
@@ -35,10 +43,14 @@ app.use(session({
 }))
 app.use(cookieParser());
 
+
 //Middleware de aplicación que se encarga de controlar si el usuario está logueado o no.
 app.use(acceso);
 
 //Middlewares que hablitan las rutas
 app.use(mainRouter); 
+app.use('/product', productRouter); 
 app.use(userRouter); 
-app.use(productRouter); 
+app.use(shoppingCartRouter);
+
+app.use('/admin/product', adminProductRoutes);
